a249cdf349b62e380fe60c06faa6a3f6fc18d6ec
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-06-12 21:21:26 -0600

    XD

--- paginas/catalogo-artesanal.php
+++ paginas/catalogo-artesanal.php
@@ -7,6 +7,7 @@
     <title>Catalogo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../Estilos/estilos.css">
+    <link rel="stylesheet" href="../Estilos/catalogo.css">
 </head>
 <body>
     <header>
@@ -41,7 +42,7 @@
         $usuario    = "id20898695_tintor";
         $contrasena = "Tintor1234@";
         try{
-            $conn = new PDO("mysql:host=$host;dbname=;charset=utf8", $usuario, $contrasena);
+            $conn = new PDO("mysql:host=$host;dbname=$database", $usuario, $contrasena);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(PDOException $e)
