65f621690f175e4845458bb97ff5573fb2f64f86
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-06-13 15:26:46 -0600

    XD

--- paginas/catalogo-tintor.php
+++ paginas/catalogo-tintor.php
@@ -58,7 +58,17 @@
         }
         ?>
     </div>
-    
+    <footer>
+        <hr>
+            <a href="https://www.facebook.com/tintorboots/?locale=es_LA">
+                <img class="instaYface" src="../imagenes/facebook.png" alt="Facebook">
+            </a>
+            <a href="https://www.instagram.com/tintorboots/">
+                <img class="instaYface" src="../imagenes/Instagram.png" alt="Instagram">
+            </a>
+        <hr>
+        <p>Copyright © 2023 Grupo Tintor S.A de C.V. </p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
